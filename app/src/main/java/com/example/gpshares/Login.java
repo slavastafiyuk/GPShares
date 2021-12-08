949ec82cc99c2e9c15fa4aac44dcbe9ba5c5fd57
slavastafiyuk <slavast@outlook.pt>
2021-12-08 16:30:26 +0000

    Initial commit

---
+++ app/src/main/java/com/example/gpshares/Login.java
@@ -0,0 +1,18 @@
+package com.example.gpshares;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        Button btnLogin = findViewById(R.id.LOGIN);
+        btnLogin.setOnClickListener(v -> startActivity(new Intent(Login.this, Map.class)));
+    }
+}
---
+++ app/src/main/java/com/example/gpshares/Registrar.java
@@ -0,0 +1,14 @@
+package com.example.gpshares;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Registrar extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registrar);
+    }
+}
