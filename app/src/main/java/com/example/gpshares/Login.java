9a727f387abd289202d30a1c75dfa9a7ff60326b
DiogoFMFurtado <70411358+DiogoFMFurtado@users.noreply.github.com>
2021-12-08 19:04:54 +0000

    toolbar

--- app/src/main/java/com/example/gpshares/Login.java
+++ app/src/main/java/com/example/gpshares/Login.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 public class Login extends AppCompatActivity {
 
