e6eed515f9b83f38a08aaff079cd52428e6b1c30
slavastafiyuk <slavast@outlook.pt>
2021-12-14 20:09:58 +0000

    Update Settings

--- app/src/main/java/com/example/gpshares/Setting.java
+++ app/src/main/java/com/example/gpshares/Setting.java
@@ -7,18 +7,37 @@ import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.UserHandle;
 import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class Setting extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
     //SideMenu--------------------------------------------------------------------------------------
     DrawerLayout drawerLayout;
     NavigationView navigationView;
     Toolbar toolbar;
+
+    private FirebaseUser user;
+    private DatabaseReference reference;
+    private String userID;
+
+    @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,7 +51,36 @@ public class Setting extends AppCompatActivity implements NavigationView.OnNavig
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.menu_Open, R.string.menu_Close);
         drawerLayout.addDrawerListener(toggle);
         toggle.syncState();
-
+        //Display user info
+        user = FirebaseAuth.getInstance().getCurrentUser();
+        reference = FirebaseDatabase.getInstance().getReference("Users");
+        userID = user.getUid();
+        final TextView fullNameTextView = (TextView) findViewById(R.id.textViewName);
+        final TextView emailTextView = (TextView) findViewById(R.id.textViewEmail);
+        final TextView ageTextView = (TextView) findViewById(R.id.textViewAge);
+        GoogleSignInAccount signInAccount = GoogleSignIn.getLastSignedInAccount(this);
+        reference.child(userID).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                Utilizador userProfile = snapshot.getValue(Utilizador.class);
+                if (userProfile != null) {
+                    String fullname = userProfile.nomeInteiro;
+                    String email = userProfile.email;
+                    String age = userProfile.idade;
+                    fullNameTextView.setText(fullname);
+                    emailTextView.setText(email);
+                    ageTextView.setText(age);
+                }else if (signInAccount != null){
+                    fullNameTextView.setText(signInAccount.getDisplayName());
+                    emailTextView.setText(signInAccount.getEmail());
+                    ageTextView.setText("Não conseguimos obter esta informação");
+                }
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Toast.makeText(Setting.this, "Algo correu mal a processar os seus dados", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     @Override
@@ -43,7 +91,6 @@ public class Setting extends AppCompatActivity implements NavigationView.OnNavig
             super.onBackPressed();
         }
     }
-
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
         switch (item.getItemId()){
