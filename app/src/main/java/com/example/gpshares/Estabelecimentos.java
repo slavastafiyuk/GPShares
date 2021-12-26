46342f93708b0f31b97628806f6ce7cd3e8b9b3e
slavastafiyuk <slavast@outlook.pt>
2021-12-26 15:40:48 +0000

    Updates

--- app/src/main/java/com/example/gpshares/Estabelecimentos.java
+++ app/src/main/java/com/example/gpshares/Estabelecimentos.java
@@ -3,7 +3,7 @@ package com.example.gpshares;
 import com.google.android.gms.maps.model.LatLng;
 
 public class Estabelecimentos {
-    public String avaliacao, comentario;
+    public String avaliacao, comentario, nome;
     public double latitude;
     public double longitude;
 
@@ -11,7 +11,8 @@ public class Estabelecimentos {
 
     }
 
-    public Estabelecimentos( String avaliacao, String comentario, double latitude, double longitude){
+    public Estabelecimentos(String nome, String avaliacao, String comentario, double latitude, double longitude){
+        this.nome = nome;
         this.avaliacao = avaliacao;
         this.comentario = comentario;
         this.latitude = latitude;
