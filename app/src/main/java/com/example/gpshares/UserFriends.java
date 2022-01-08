0a5e8f44e41cce4638bcbb3019eb01739cdbb8d2
slavastafiyuk <slavast@outlook.pt>
2022-01-08 16:02:50 +0000

    Updates

--- app/src/main/java/com/example/gpshares/UserFriends.java
+++ app/src/main/java/com/example/gpshares/UserFriends.java
@@ -7,9 +7,9 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.view.View;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.example.gpshares.FriendsHelper.Friends;
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
